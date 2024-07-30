8bf35310ea3212772ae34eb19937d8efbe36c995
Ahmed Youssef <My_desktop@live.com>
2024-07-31 00:08:06 +0300

    sort the People problem

--- src/LeetCode/SortThePeople/runner.java
+++ src/LeetCode/SortThePeople/runner.java
@@ -1,11 +1,12 @@
-import java.util.*;
-import java.util.stream.Collectors;
+package LeetCode.SortThePeople;
+
+import java.util.TreeMap;
 
 ////////Input text and output Array of string sorted Desc ////////////
 
 public class runner {
 
-    public static String longestCommonPrefix(String[] names, int[] height) {
+    public static String sortPeople(String[] names, int[] height) {
         TreeMap<Integer, String> combinedMap = new TreeMap<>();
         for (int i = 0; i < names.length; i++) {
             combinedMap.put(height[i], names[i]);
@@ -15,7 +16,7 @@ public class runner {
 
 
     public static void main(String[] args) {
-        System.out.println(longestCommonPrefix(new String[]{"Mary", "John", "Emma"}, new int[]{180, 165, 170}));
-        System.out.println(longestCommonPrefix(new String[]{"Alice","Bob","Bob"}, new int[]{155,185,150}));
+        System.out.println(sortPeople(new String[]{"Mary", "John", "Emma"}, new int[]{180, 165, 170}));
+        System.out.println(sortPeople(new String[]{"Alice","Bob","Bob"}, new int[]{155,185,150}));
     }
 }
